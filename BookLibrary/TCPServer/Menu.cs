046f52ea09fdec06865ae0fc971401f5335ede47
Aukse Matulaite <auks0017@edu.easj.dk>
2020-10-25 16:44:05 +0100

    TCPServer

---
+++ BookLibrary/TCPServer/Menu.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPServer
+{
+    class Menu
+    {
+        public static void MainMenu()
+        {
+            Boolean status = true;
+
+            while (status)
+            {
+                Console.WriteLine("Give your choice 1,2,3,9:");
+                Console.WriteLine("1: Get all books");
+                Console.WriteLine("2: Get a book by Isbn number");
+                Console.WriteLine("3: Add a new book");
+
+                Console.WriteLine("9: Exit program");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine("Key:" + key);
+
+                if (char.IsDigit(key.KeyChar))
+                {
+                    int choice = Convert.ToInt32(key.KeyChar) - 48;
+
+                    Console.WriteLine("choice:" + choice);
+                    switch (choice)
+                    {
+                        case 1:
+                            menu1(); break;
+                        case 2:
+                            menu2(); break;
+                        case 3:
+                            menu3(); break;
+                        default:
+                            Console.WriteLine("Sorry wrong number not (1,2,3,4,9) ");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nSorry, you need to input a number");
+                }
+            }
+        }
+
+        private static void menu1()
+        {
+            IList<Book> cList = TaskController.GetBookAsync().Result;
+            Console.WriteLine(string.Join("\n", cList.ToString()));
+            //Fast write out
+            for (int i = 0; i < cList.Count; i++)
+                Console.WriteLine(cList[i].ToString());
+            Console.WriteLine();
+        }
+
+
+        private static void menu2()
+        {
+            try
+            {
+                Console.WriteLine("GET Give Isbn number of the book");
+                string isbn13 = Console.ReadLine();
+                int id = int.Parse(isbn13);
+                Book cb = TaskController.GetOneBookAsync1(isbn13).Result;
+                Console.WriteLine(cb.ToString());
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private static void menu3()
+        {
+            Console.WriteLine("POST: Give data for book to be inserted");
+            Console.WriteLine("Title: ");
+            String title = Console.ReadLine();
+            Console.WriteLine("Author: ");
+            String author = Console.ReadLine();
+            Console.WriteLine("Page Number: ");
+            String Pagenr = Console.ReadLine();
+            int pages = Int32.Parse(Pagenr);
+            Console.WriteLine("Isbn13 number: ");
+            String Isbn13 = Console.ReadLine();
+
+            Book newBook = new Book(title, author, pages, Isbn13);
+            Book book = TaskController.AddBookAsync(newBook).Result;
+            Console.WriteLine("Book inserted");
+            Console.WriteLine(book.ToString());
+        }
+    }
+}
---
+++ BookLibrary/TCPServer/TaskController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BookLibrary;
+using Newtonsoft.Json;
+
+namespace TCPServer
+{
+    class TaskController
+    {
+        private const string BooksUri = "https://localhost:44354/api/Books";
+
+        public TaskController() { }
+
+        public static async Task<IList<Book>> GetBookAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(BooksUri);
+                IList<Book> cList = JsonConvert.DeserializeObject<IList<Book>>(content);
+                return cList;
+            }
+        }
+
+        public static async Task<Book> GetOneBookAsync(string Isbn13)
+        {
+            string requestUri = BooksUri + "/" + Isbn13;
+            using (HttpClient client = new HttpClient())
+            {
+                string content = await client.GetStringAsync(requestUri);
+                Book c = JsonConvert.DeserializeObject<Book>(content);
+                return c;
+            }
+        }
+
+        public static async Task<Book> GetOneBookAsync1(string Isbn13)
+        {
+            string requestUri = BooksUri + "/" + Isbn13;
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new Exception("Book not found");
+
+                }
+                response.EnsureSuccessStatusCode();
+                string str = await response.Content.ReadAsStringAsync();
+                Book c = JsonConvert.DeserializeObject<Book>(str);
+                return c;
+            }
+        }
+
+        public static async Task<Book> AddBookAsync(Book newBook)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                var jsonString = JsonConvert.SerializeObject(newBook);
+                Console.WriteLine("JSON: " + jsonString);
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(BooksUri, content);
+                if (response.StatusCode == HttpStatusCode.Conflict)
+                {
+                    throw new Exception("Book already exists");
+                }
+                response.EnsureSuccessStatusCode();
+                string str = await response.Content.ReadAsStringAsync();
+                Book copyOfNewBook = JsonConvert.DeserializeObject<Book>(str);
+                return copyOfNewBook;
+            }
+
+        }
+    }
+}
